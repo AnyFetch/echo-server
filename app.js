77dc31b3327ba2306e5b657ea94fc6ccb65e7958
Neamar <neamar@neamar.fr>
2014-11-24 17:11:02 +0100

    And we're done

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 "use strict";
 
 var restify = require('restify');
+var document = require('./lib/handlers/document.js');
 
 var server = restify.createServer({
   name: 'echo-server',
   version: '1.0.0',
 });
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+
+server.get('/:id', document.get);
+server.post('/:id', document.post);
+server.patch('/:id', document.post);
 
 
 module.exports = server;
--- test/index.js
+++ test/index.js
@@ -8,11 +8,12 @@ var server = require('../app.js');
 describe("Echo server", function() {
   it("should accept POST queries", function(done) {
     request(server)
-      .post('/123')
+      .post('/123?get_param=foo')
       .expect(200)
       .send({
-        foo: 'bar'
+        post_param: 'bar'
       })
+      .expect(/stored/)
       .end(done);
   });
 
@@ -21,8 +22,33 @@ describe("Echo server", function() {
       .get('/123')
       .expect(200)
       .expect(function(res) {
-        console.log(res.body);
+        res.body.should.have.property('url', '/123?get_param=foo');
+        res.body.should.have.property('headers').and.have.property('content-length');
+        res.body.should.have.property('query').and.have.property('get_param', 'foo');
+        res.body.should.have.property('body').and.have.property('post_param', 'bar');
       })
+      .end(done);
+  });
+
+  it("should accept PATCH queries", function(done) {
+    request(server)
+      .patch('/456')
+      .expect(200)
+      .send({
+        post_param: 'bar'
+      })
+      .expect(/stored/)
+      .end(done);
+  });
+
+  it("should let the user override the response", function(done) {
+    request(server)
+      .patch('/456')
+      .expect(200)
+      .send({
+        _echo_reply: 'foobar'
+      })
+      .expect(/foobar/)
       .end(done);
   });
 });

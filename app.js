b22ccbbf081f55797c905cb8a2a54aad993e3958
Neamar <neamar@neamar.fr>
2014-11-27 15:09:44 +0100

    Index route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('heroku-self-ping')(process.env.ECHO_URL);
 
 var restify = require('restify');
 var document = require('./lib/handlers/document.js');
+var index = require('./lib/handlers/index.js');
 
 
 var server = restify.createServer({
@@ -14,6 +15,7 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 
+server.get('/', index.get);
 server.get('/:id', document.get);
 server.post('/:id', document.post);
 server.patch('/:id', document.patch);
